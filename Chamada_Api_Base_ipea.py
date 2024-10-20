7379ac1cefd86e4a7ba668336cb3e826199c4366
Marcos Gregorios <61382919+Gregoriodata@users.noreply.github.com>
2024-10-20 18:43:29 -0300

    Chamapa API IPEA - V1

---
+++ Chamada_Api_Base_ipea.py
@@ -0,0 +1,19 @@
+import requests as rq
+import json
+import pandas as pd
+
+# URL da API pública -
+# Feminicíos mulheres Negras  - 142
+# Abrangencia - UF
+
+
+url = "https://www.ipea.gov.br/atlasviolencia/api/v1/valores-series/142/3"
+
+# Fazer a requisição GET para a API
+response = rq.request("get", url)  # Requisição API metodo GET
+# Converter os dados em dicionario text - metodo
+retor_requi = json.loads(response.text)
+df = pd.DataFrame(retor_requi)  # Transformar dic em Data Frame
+
+
+print(f"Erro na requisição: {response.status_code}")
